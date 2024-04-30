9a10e868cef621c9d2534646d02c08ae7d4a9795
tayadeabhilash <tayadeabhilash@gmail.com>
2024-04-30 16:07:43 -0700

    Weekly history feature

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, render_template, redirect, url_for
-from finance_info import allocateFunds, queryStockInfoMultiple
+from finance_info import allocateFunds, queryStockInfoMultiple, get_historical_data
+
 # import dotenv
 
 app = Flask('__name__')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def calculator():
     if request.method == 'POST':
-        strategies = request.form.getlist('strategies[]', type=str)
+        strategies = request.form.getlist('selected_strategies', type=str)
         investment_amount = int(request.form.get('investment_amount', 0))
         if len(strategies) > 0:
-            return redirect(url_for('display_portafolio', strategies=",".join(strategies), investment_amount=investment_amount))
+            return redirect(
+                url_for('display_portafolio', strategies=",".join(strategies), investment_amount=investment_amount))
     return render_template('index.html')
 
 
@@ -18,11 +21,14 @@ def calculator():
 def display_portafolio():
     strategies = request.args.get('strategies', type=str).split(',')
     investment_amount = request.args.get('investment_amount', type=int)
-    
+
     stocks_info_from_api = queryStockInfoMultiple(strategies=strategies)
     stocks_info_with_allocation = allocateFunds(investment_amount, stocks_info_from_api)
-    
-    return render_template('portafolio.html', stocks_info=stocks_info_with_allocation)
+    dates, historical_data_for_stocks = get_historical_data(stocks_info_with_allocation)
+
+    return render_template('Strategies.html', stocks_info=stocks_info_with_allocation,
+                           dates=dates, historical_data=historical_data_for_stocks)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- finance_info.py
+++ finance_info.py
@@ -1,25 +1,42 @@
 import yfinance as yf
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from requests.exceptions import ConnectionError
 import json
+from prettytable import PrettyTable
 
-# pip install yfinance
-    
+
 def main():
     strategies = [
         "Ethical Investing",
-        # "Index Investing",
         "Growth Investing",
     ]
-    
+
     stocks_info = queryStockInfoMultiple(strategies=strategies)
-    pretty_json = json.dumps(stocks_info, indent=2, sort_keys=False)
-    print(pretty_json)
-    
+
     allocation = allocateFunds(10_000, stocks_info)
+    print(get_historical_data(allocation))
     pretty_json = json.dumps(allocation, indent=2, sort_keys=False)
     print(pretty_json)
+    table = PrettyTable()
+
+    # Define columns
+    table.field_names = ["Strategy Name", "Stock Name", "Symbol", "Current Price", "Allocation"]
+
+    # Add data to table
+    for strategy in allocation:
+        strategy_name = strategy["strategy_name"]
+        allocation = strategy["allocation"]
+        for stock in strategy["stocks"]:
+            stock_name = stock["longName"]
+            symbol = stock["symbol"]
+            current_price = stock["currentPrice"]
+            allocation_stock = stock["allocation"]
+            table.add_row([strategy_name, stock_name, symbol, current_price, allocation_stock])
+
+    # Print the table
+    print(table)
+
 
 def getCurrentTime():
     # Setting the timezone to Pacific Daylight Time (PDT)
@@ -29,50 +46,18 @@ def getCurrentTime():
     # Formatting the date and time as requested
     formatted_datetime = current_datetime_pdt.strftime('%a %b %d %H:%M:%S PDT %Y')
     return formatted_datetime
-    
-def queryStockInfoSingle(symbol):
-    # Create a ticker for querying stock info
-    stock = yf.Ticker(symbol)
-    try:
-        # # Get stock information
-        stock_info = stock.info
 
-        # Extract wanted data
-        longName = stock_info.get('longName', 'N/A')
-        # symbol = stock_info.get('symbol', 'N/A')
-        currentPrice = stock_info.get('currentPrice', 0)
-        previousClose = stock_info.get('previousClose', 0)
-        # Calculate difference from previous day closure
-        difference = currentPrice - previousClose
-        differencePercentage = difference / previousClose * 100
-        return {
-            'longName': longName,
-            'symbol': symbol,
-            'currentPrice': currentPrice,
-            'difference': f'{difference:+.2f}',
-            'differencePercentage': f'{differencePercentage:+.2f}',
-        }
 
-    # Catch connection error
-    except ConnectionError as e:
-        return f"Connection error occurred: {e}"
-    # Catch not found symbol error
-    except KeyError as e:
-        return f"Error: {symbol.upper()} was not found."
-    # Unexpected errors
-    except Exception as e:
-        return f"Unexpected error: {e}"
-    
 def queryStockInfoMultiple(strategies):
     investment_strategies = {
-        "Ethical Investing": ["AAPL", "ADBE", "NSRGY"],
-        "Growth Investing": ["Ticker1", "ADBE", "Ticker3"],
-        "Index Investing": ["VTI", "IXUS", "ILTB"],
-        "Quality Investing": ["Ticker4", "Ticker5", "Ticker6"],
-        "Value Investing": ["Ticker7", "Ticker8", "Ticker9"]
+        "Ethical Investing": ["TSLA", "BYND", "DANOY"],
+        "Growth Investing": ["AMZN", "GOOGL", "SQ"],
+        "Index Investing": ["VTI", "SPY", "ACWI"],
+        "Quality Investing": ["JNJ", "V", "MSFT"],
+        "Value Investing": ["BRK.A", "XOM", "WMT"]
     }
     result = []
-    
+
     for strategy in strategies:
         strategy_container = {
             'strategy_name': strategy,
@@ -81,17 +66,37 @@ def queryStockInfoMultiple(strategies):
         for stock_symbol in investment_strategies[strategy]:
             strategy_container['stocks'].append(queryStockInfoSingle(symbol=stock_symbol))
             print('Fetched stock')
-            
+
         result.append(strategy_container)
     return result
-    
+
+
+def get_previous_trading_sessions(start_date, num_sessions):
+    trading_sessions = []
+    while len(trading_sessions) < num_sessions:
+        # Check if the current day is not Saturday or Sunday
+        if start_date.weekday() < 5:
+            trading_sessions.append(start_date.strftime('%Y-%m-%d'))
+        # Move to the previous day
+        start_date -= timedelta(days=1)
+    return trading_sessions
+
 def queryStockInfoSingle(symbol):
     # Create a ticker for querying stock info
     stock = yf.Ticker(symbol)
     try:
         # # Get stock information
         stock_info = stock.info
+        # Get today's date
+        today = datetime.today().strftime('%Y-%m-%d')
 
+        # Get data for the previous 5 days
+        previous_trading_sessions = get_previous_trading_sessions(datetime.today(), 6)
+        historical_data = []
+        # Fetch historical data for each trading session
+        data = stock.history(start=previous_trading_sessions[-1], end=today)
+        for index, row in data.iterrows():
+            historical_data.append({'timestamp': index.strftime('%m/%d'), 'close': row['Close']})
         # Extract wanted data
         longName = stock_info.get('longName', 'N/A')
         # symbol = stock_info.get('symbol', 'N/A')
@@ -106,6 +111,7 @@ def queryStockInfoSingle(symbol):
             'currentPrice': currentPrice,
             'difference': f'{difference:+.2f}',
             'differencePercentage': f'{differencePercentage:+.2f}',
+            'historical_data': historical_data
         }
 
     # Catch connection error
@@ -118,9 +124,28 @@ def queryStockInfoSingle(symbol):
     except Exception as e:
         return f"Unexpected error: {e}"
 
+def get_historical_data(stocks_info_with_allocation):
+    data = {}
+    dates = []
+    total_stock_values = []
+    for strategy in stocks_info_with_allocation:
+        for stock in strategy['stocks']:
+            for date in stock['historical_data']:
+                if date['timestamp'] not in dates:
+                    dates.append(date['timestamp'])
+                if stock['symbol'] not in data:
+                    data[stock['symbol']] = {'values': [], 'total_values': []}
+
+            for date in stock['historical_data']:
+                data[stock['symbol']]['values'].append(date['close'])
+                data[stock['symbol']]['total_values'].append(date['close']*stock['units'])
+
+    return dates, data
+
+
 def allocateFunds(investment_amount, strategies_info):
     total_strategy_weight, stock_weights_by_strategy = calculateWeights(strategies_info)
-    
+
     # Assign funds based on calculated weights
     for strategy, weights in zip(strategies_info, stock_weights_by_strategy):
         strategy_weight, strategy_stock_weights, total_stock_weight = weights
@@ -130,8 +155,10 @@ def allocateFunds(investment_amount, strategies_info):
             for stock, stock_weight in zip(strategy['stocks'], strategy_stock_weights):
                 if isinstance(stock, dict):
                     stock['allocation'] = (stock_weight / total_stock_weight) * strategy_allocation
+                    stock['units'] = stock['allocation'] / stock['currentPrice']
     return strategies_info
-    
+
+
 def calculateWeights(strategies_info):
     # Define fixed weights for strategies and stocks
     strategies_weights = getStrategyWeights()
@@ -157,25 +184,32 @@ def calculateWeights(strategies_info):
         stock_weights_by_strategy.append((strategy_weight, strategy_stock_weights, total_stock_weight))
     return total_strategy_weight, stock_weights_by_strategy
 
+
 def getStockWeights():
     stocks_weights = {
-        "AAPL": 5,
-        "ADBE": 7,
-        "NSRGY": 6,
-        "VTI": 4,
-        "ILTB": 7,
+        "TSLA": 7,
+        "BYND": 5,
+        "DANOY": 6,
+        "VTI": 8,
+        "SPY": 7,
+        "ACWI": 7,
+        "AMZN": 7,
+        "GOOGL": 7,
+        "SQ": 6,
     }
     return stocks_weights
+
 
 def getStrategyWeights():
     strategies_weights = {
         "Ethical Investing": 5,
         "Growth Investing": 7,
-        "Index Investing": 6,
+        "Index Investing": 10,
         "Quality Investing": 8,
-        "Value Investing": 10
+        "Value Investing": 6
     }
     return strategies_weights
-   
+
+
 if __name__ == '__main__':
     main()
